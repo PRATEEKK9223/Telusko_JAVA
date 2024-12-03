0c3fd968b2a47dcf168ef91f741e82a57b1a49df
PRATEEKK9223 <pratheekkpratheekk@gmail.com>
2024-12-03 08:58:37 +0530

    3-12-24

---
+++ SUPPER.java
@@ -0,0 +1,39 @@
+// in java Each Constructor having one method called super() this will calls the constructor of parent class(this is default method no need to explisity add )
+//this() fuction is mention exiplisitly to call itself construction 
+
+
+class A extends Object //every class extends this Object class defaultly
+{
+    public A(){
+        super();
+        System.out.println("in A");
+    }
+
+    public A(int A){
+        super();
+        System.out.println("in A int");
+
+    }
+}
+
+class B extends A
+{
+    public B(){
+        super();
+        System.out.println("in B");
+    }
+    public B(int n){
+        super(2);
+        System.out.println("in B int");
+    }
+}
+
+
+
+public class SUPPER 
+{
+    public static void main(String[] arg){
+        B obj =new B(3);
+
+    }
+}
