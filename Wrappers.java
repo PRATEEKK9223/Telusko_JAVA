da007280d2dbf4e8f7e6a2bb9296fa1ffb560302
darshan <pratheekkpratheekk@gamil.com>
2024-12-09 23:04:15 +0530

    commited

--- Wrappers.java
+++ Wrappers.java
@@ -1,15 +1,20 @@
 public class Wrappers {
     int num=10;
     Integer num1=new Integer(num);//boxing
-    Integer num2=num; //Auto-boxing
+    Integer num2=num; //Auto-boxing (the premitive values are stored in a object type)
 
     //Auto-unboxing(fetching the premitive data from object type )
     int res=num2;
+    String str="12";
+    int N=Integer.parseInt(str);
+
 
     public void show(){
         System.out.println(num1);
         System.out.println(num2);
         System.out.println(res);
+        System.out.println(N*2);
+        
 
     }
 
